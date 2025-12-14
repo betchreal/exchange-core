d988db8076d2da6f6017c572e37561f52be9a906
betchreal <thebrainsir931@gmail.com>
2025-12-14 01:40:39 +0100

    fix(app): fixed incorrect behaviour of browsers (Safari, etc) and mobile versions with incorrect identifying different domains of client and server

--- apps/web/lib/axios.ts
+++ apps/web/lib/axios.ts
@@ -1,13 +1,7 @@
 import axios from "axios";
 
-const apiUrl = process.env.NEXT_PUBLIC_API_URL;
-
-if (!apiUrl) {
-  throw new Error("NEXT_PUBLIC_API_URL is not set");
-}
-
 export const api = axios.create({
-  baseURL: apiUrl,
+  baseURL: "/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
